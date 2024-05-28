b3f90d5e0439be9d306357435099f441916fc3b5
Etienne Bosse <etienne@secretarium.org>
2024-05-28 13:54:07 +0100

    Return json string from insert-in-ledger

--- apps/rust-template/src/lib.rs
+++ apps/rust-template/src/lib.rs
@@ -15,7 +15,6 @@ impl Guest for Component {
     }
 
     fn load_from_ledger(cmd: String){
-        sdk::notify(&format!("received command {}", cmd));
         let Ok(v) = serde_json::from_str::<Value>(&cmd) else {
             sdk::notify_error(&format!("failed to parse '{}' as json", cmd));
             return
@@ -34,7 +33,6 @@ impl Guest for Component {
     }
 
     fn insert_in_ledger(cmd: String){
-        sdk::notify(&format!("received command {}", cmd));
         let Ok(v) = serde_json::from_str::<Value>(&cmd) else {
             sdk::notify_error(&format!("failed to parse '{}' as json", cmd));
             sdk::cancel_transaction();
@@ -42,8 +40,13 @@ impl Guest for Component {
         };
         let key = v["key"].as_str().unwrap().as_bytes();
         let value = v["value"].as_str().unwrap().as_bytes();
-        sdk::notify(&format!("about to call write_ledger(\"my_table\", {:?}, {:?})", key, value));
         sdk::write_ledger("my_table", key, value);
+        let result_as_json = json!({
+            "inserted": true,
+            "key": key,
+            "value": value
+            });
+        sdk::notify(&result_as_json.to_string());
     }
 
     fn ping() {
