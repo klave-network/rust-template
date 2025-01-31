7be9dbbc9809039b24eda3682948325f5d26fba5
Jeremie Labbe <jeremie@secretarium.org>
2025-01-31 16:09:58 +0000

    Update with crates.io Klave SDK

--- apps/rust-template/src/lib.rs
+++ apps/rust-template/src/lib.rs
@@ -1,7 +1,8 @@
 #[allow(warnings)]
 mod bindings;
 
-use bindings::{Guest, klave::sdk::sdk};
+use bindings::Guest;
+use klave;
 use serde_json::Value;
 use serde_json::json;
 struct Component;
@@ -9,45 +10,45 @@ struct Component;
 impl Guest for Component {
 
     fn register_routes(){
-        sdk::add_user_query("load-from-ledger");
-        sdk::add_user_transaction("insert-in-ledger");
-        sdk::add_user_query("ping");
-        sdk::add_user_query("ping2");
+        klave::router::add_user_query("load-from-ledger");
+        klave::router::add_user_transaction("insert-in-ledger");
+        klave::router::add_user_query("ping");
+        klave::router::add_user_query("ping2");
     }
 
     fn load_from_ledger(cmd: String){
         let Ok(v) = serde_json::from_str::<Value>(&cmd) else {
-            sdk::notify_error(&format!("failed to parse '{}' as json", cmd));
+            klave::notifier::notify_error(&format!("failed to parse '{}' as json", cmd));
             return
         };
-        let key = v["key"].as_str().unwrap().as_bytes();
-        let Ok(res) = sdk::read_ledger("my_table", key) else {
-            sdk::notify_error(&format!("failed to read from ledger: '{}'", cmd));
+        let key = v["key"].as_str().unwrap();
+        let Ok(res) = klave::ledger::get_table("my_table").get(key) else {
+            klave::notifier::notify_error(&format!("failed to read from ledger: '{}'", cmd));
             return
         };
         let msg = if res.is_empty() {
             format!("the key '{}' was not found in table my_table", cmd)
         } else {
             let result_as_json = json!({
-                "value": String::from_utf8(res).unwrap_or("!! utf8 parsing error !!".to_owned()),
+                "value": res,
             });
             format!("{}", result_as_json.to_string())
         };
-        sdk::notify(&msg);
+        klave::notifier::notify(&msg);
     }
 
     fn insert_in_ledger(cmd: String){
         let Ok(v) = serde_json::from_str::<Value>(&cmd) else {
-            sdk::notify_error(&format!("failed to parse '{}' as json", cmd));
-            sdk::cancel_transaction();
+            klave::notifier::notify_error(&format!("failed to parse '{}' as json", cmd));
+            // klave:: cancel_transaction();
             return
         };
-        let key = v["key"].as_str().unwrap().as_bytes();
-        let value = v["value"].as_str().unwrap().as_bytes();
-        match sdk::write_ledger("my_table", key, value) {
+        let key = v["key"].as_str().unwrap();
+        let value = v["value"].as_str().unwrap();
+        match klave::ledger::get_table("my_table").set(key, value) {
             Err(e) => {
-                sdk::notify_error(&format!("failed to write to ledger: '{}'", e));
-                sdk::cancel_transaction();
+                klave::notifier::notify_error(&format!("failed to write to ledger: '{}'", e));
+                // sdk::cancel_transaction();
                 return
             }
             _ => {}
@@ -58,15 +59,15 @@ impl Guest for Component {
             "key": key,
             "value": value
             });
-        sdk::notify(&result_as_json.to_string());
+        klave::notifier::notify(&result_as_json.to_string());
     }
 
     fn ping() {
-        sdk::notify("pong");
+        klave::notifier::notify("pong");
     }
 
     fn ping2() {
-        sdk::notify("pang2");
+        klave::notifier::notify("pang2");
     }
 }
 
